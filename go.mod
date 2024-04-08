08f1e5a49d032ff7cdffe19e30ac71905c46af53
Conor McKeever <cmckeever@novacoast.com>
2024-04-08 13:11:41 +0100

    Add scan code and main for testing

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module checkout
+module main
 
 go 1.19
--- main.go
+++ main.go
@@ -1,4 +1,13 @@
-package checkout
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 type ICheckout interface {
 	Scan(item string) error
@@ -28,4 +37,33 @@ func NewCheckout() *Checkout {
 		"B": {quantity: 2, price: 45},
 	}
 	return &Checkout{itemPrices: itemPrices, deals: deals, cart: make(map[string]int)}
+}
+
+func (c *Checkout) Scan(item string) error {
+	if _, ok := c.itemPrices[item]; !ok {
+		return errors.New("invalid item")
+	}
+	c.cart[item]++
+	log.Printf("Adding item: %v to cart", item)
+	return nil
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	checkout := NewCheckout()
+
+	fmt.Println("Enter SKUs to scan:")
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		err := checkout.Scan(line)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+	}
+
+	if scanner.Err() != nil {
+		fmt.Printf("Error reading input: %v\n", scanner.Err())
+	}
 }
