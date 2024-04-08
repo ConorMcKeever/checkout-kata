3a09164bc53fbcc4f2cdc18fe72f9c5effd65b36
Conor McKeever <cmckeever@novacoast.com>
2024-04-08 13:01:52 +0100

    Set up structs and checkout object

--- main.go
+++ main.go
@@ -4,3 +4,28 @@ type ICheckout interface {
 	Scan(item string) error
 	GetTotalPrice() int
 }
+
+type Deal struct {
+	quantity int
+	price    int
+}
+
+type Checkout struct {
+	itemPrices map[string]int
+	deals      map[string]Deal
+	cart       map[string]int
+}
+
+func NewCheckout() *Checkout {
+	itemPrices := map[string]int{
+		"A": 50,
+		"B": 30,
+		"C": 20,
+		"D": 15,
+	}
+	deals := map[string]Deal{
+		"A": {quantity: 3, price: 130},
+		"B": {quantity: 2, price: 45},
+	}
+	return &Checkout{itemPrices: itemPrices, deals: deals, cart: make(map[string]int)}
+}
