59c80c13f4b50d9aa62abf409a400de5c1ac7dd7
Conor McKeever <cmckeever@novacoast.com>
2024-04-08 13:58:31 +0100

    Add finish to main

--- main.go
+++ main.go
@@ -16,12 +16,14 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		line = strings.TrimSpace(line)
-
+		if line == "finish" {
+			fmt.Printf("Thank you for checking out, your final total is: %d\n", checkout.GetTotalPrice())
+			break
+		}
 		if line == "total" {
 			fmt.Printf("Total: %d\n", checkout.GetTotalPrice())
 			continue
 		}
-
 		err := checkout.Scan(line)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
